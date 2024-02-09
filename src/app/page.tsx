bdff93c935782b5758bfc417bcbc9db7b75a1637
Isaac Dyor <isaac@dyor.com>
2024-02-08 18:05:01 -0800

    adding shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { UserButton, SignUp } from "@clerk/nextjs";
 import { Sign } from "crypto";
 
 export default function Home() {
   return (
     <div className="h-screen">
+      <Button>Click me</Button>
       <SignUp />
       <UserButton afterSignOutUrl="/" />
     </div>
