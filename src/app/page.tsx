65e48f98e67cbc1597ec277fad4fc8e9e5d2d6f5
Isaac Dyor <isaac@dyor.com>
2024-02-08 18:22:05 -0800

    supress hydration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { UserButton, SignUp } from "@clerk/nextjs";
+import { UserButton, SignUp, SignUpButton } from "@clerk/nextjs";
 import { Sign } from "crypto";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <Button>Click me</Button>
-      <SignUp />
+      <SignUpButton />
       <UserButton afterSignOutUrl="/" />
     </div>
   );
